610a6e91dbe01b1477a349d33fb70e04792ff3c8
shrikarthik07 <shrikarthik@loyalytics.in>
2025-06-18 23:55:54 +0530

    Add .gitignore, dotenv dependency, and configure database connection with environment variables

--- config/database.js
+++ config/database.js
@@ -1,8 +1,23 @@
-const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Load .env variables
 
-const sequelize = new Sequelize('your_db', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+                ca: fs.readFileSync(path.resolve(__dirname, '..', process.env.DB_CA_CERT_PATH)).toString()
+            }
+        },
+        logging: false,
+    }
+);
 
 module.exports = sequelize;
